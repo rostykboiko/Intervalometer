37c3118e12d9a50aa4ff5b816e71750e16ef13ec
rostykboiko <rosstyk@gmail.com>
2017-12-17 19:48:29 +0200

    Bug Fix

--- app/src/main/java/com/example/rosst/intervalometer/main/MainActivity.java
+++ app/src/main/java/com/example/rosst/intervalometer/main/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             initializeView();
         }
-        selectCameraApp();
+        setCameraAppTitle();
     }
 
     @OnClick(R.id.shortcut)
